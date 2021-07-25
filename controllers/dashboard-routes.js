10816cc06d2aa120b9a2e615e3a28791a0bee6fa
alexelliott22 <81639053+alexelliott22@users.noreply.github.com>
2021-07-25 17:34:22 -0400

    dumb changes

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -4,7 +4,6 @@ const { Artwork, Museum, Artist} = require('../models');
 //get all posts for an individual user
 router.get('/', (req, res) => {
     Artwork.findAll({
-<<<<<<< HEAD
       where: {
         user_id: req.session.user_id
       },
@@ -27,36 +26,11 @@ router.get('/', (req, res) => {
             attributes: ['artist_name']
         }
       ]
-=======
-      //COMMENTED OUT SECTION SO I COULD TEST W/O AUTHENTICATION
-      // where: {
-      //   user_id: req.session.user_id
-      // },
-      // attributes: [
-      //   'id',
-      //   'title',
-      //   'medium',
-      //   'created_at',
-      //   'date',
-      //   'style',
-      //   'location'
-      // ],
-      // include: [
-      //   {
-      //     model: Museum,
-      //     attributes: ['username']
-
-      //   },
-      //   {
-      //       model: Artist,
-      //       attributes: 'artist_name'
-      //   }
-      // ]
->>>>>>> main
     })
     .then(dbPostData => {
         // serialize data before passing to template
         const posts = dbPostData.map(post => post.get({ plain: true }));
+        console.log(posts);
         res.render('artwork-dashboard', { posts, loggedIn: true, layout: 'dashboard'});
     })
     .catch(err => {
