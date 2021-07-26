ec6cbae4061e2345377e641b5dd07967ca8e0d4f
alexelliott22 <81639053+alexelliott22@users.noreply.github.com>
2021-07-26 19:59:42 -0400

    add artwork and artist js files

---
+++ public/javascript/add-artist.js
@@ -0,0 +1,32 @@
+const addArtist = (e) => {
+    try {
+        e.preventDefault();
+
+        const artist_name = document.querySelector('#artist-name').value.trim();
+        const birth_date = document.querySelector('#birthDate').value.trim();
+        const death_date = document.querySelector('#deathDate').value.trim();
+
+        if(artist_name) {
+            const response = await fetch('/api/artwork', {
+                method: 'post',
+                body: JSON.stringify({
+                    artist_name,
+                    birth_date,
+                    death_date
+                }),
+                headers: { 'Content-Type': 'application/json' }
+            });
+            
+            if (response.ok) {
+                document.location.replace('/');
+            } else {
+                alert(response.statusText);
+            }
+        }
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+document.querySelector('#add-artist').addEventListener('click', addArtist);
