0b84777ffc001b99d3cae38a3f686936ae18abc7
alexelliott22 <81639053+alexelliott22@users.noreply.github.com>
2021-07-29 19:27:06 -0400

    add single route

---
+++ public/javascript/single-artwork.js
@@ -0,0 +1,9 @@
+const getSingleArtwork = (e) => {
+    
+
+    console.log(e.path[1]);
+    console.log('here')
+    // const artworkID = 
+}
+
+document.querySelector('.card').addEventListener('click', getSingleArtwork);
