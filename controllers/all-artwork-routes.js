760543bae575f3aa088a9513d8f0c8545fb8add0
alexelliott22 <81639053+alexelliott22@users.noreply.github.com>
2021-07-31 12:41:14 -0400

    clean up controllers file

--- controllers/all-artwork-routes.js
+++ controllers/all-artwork-routes.js
@@ -29,16 +29,6 @@ router.get('/', async (req, res) => {
                         }
                     ]
                 },
-                attributes: [
-                    'id',
-                    'title',
-                    'medium',
-                    'created_at',
-                    'date',
-                    'style',
-                    'location',
-                    'image_url'
-                ],
                 include: [
                     {
                         model: Museum,
@@ -52,16 +42,6 @@ router.get('/', async (req, res) => {
             })
         } else {
             artwork = await Artwork.findAll({
-                attributes: [
-                    'id',
-                    'title',
-                    'medium',
-                    'created_at',
-                    'date',
-                    'style',
-                    'location',
-                    'image_url'
-                ],
                 include: [
                     {
                         model: Museum,
@@ -78,7 +58,7 @@ router.get('/', async (req, res) => {
 
         //pass a single post object into the homepage
         const posts = artwork.map(post => post.get({ plain: true }))
-        res.render('all-artwork', {posts});
+        res.render('all-artwork', {posts, loggedIn: req.session.loggedIn});
 
     } catch (error) {
         console.log('Failed to get posts');
@@ -87,51 +67,5 @@ router.get('/', async (req, res) => {
         
     }    
 });
-
-router.get('/single-artwork/:id', (req, res) => {
-    Artwork.findOne({
-        where: {
-            id: req.params.id
-        },attributes: [
-            'id',
-            'title',
-            'medium',
-            'created_at',
-            'date',
-            'style',
-            'location',
-            'image_url'
-        ],
-        include: [
-            {
-                model: Museum,
-                attributes: ['username']
-            },
-            {
-                model: Artist,
-                attributes: ['artist_name']
-            }
-        ]
-    })
-    .then(dbPostData => {
-        if (!dbPostData) {
-            res.status(404).json({ message: 'No post found with this id' });
-            return;
-        }
-
-        // serialize the data
-        const post = dbPostData.get({ plain: true });
-
-        // pass data to template
-        //can we change to single-artwork
-        res.render('single-artwork', {
-            post
-        });
-    })
-    .catch(err => {
-        console.log('failed to get post');
-        res.status(500).json(err);
-    });
-})
 
 module.exports = router;
--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -8,16 +8,6 @@ router.get('/', (req, res) => {
       where: {
         museum_id: req.session.museum_id
       },
-      attributes: [
-        'id',
-        'title',
-        'medium',
-        'created_at',
-        'date',
-        'style',
-        'location',
-        'image_url'
-      ],
       include: [
         {
           model: Museum,
@@ -40,53 +30,5 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
     });
 });
-
-//this route gives the user the ability to edit a post
-router.get('/edit/:id', (req, res) => {
-    Artwork.findOne({
-      where: {
-        id: req.params.id
-      },
-      attributes: [
-        'id',
-        'title',
-        'medium',
-        'created_at',
-        'date',
-        'style',
-        'location',
-        'image_url'
-      ],
-      include: [
-        {
-          model: Museum,
-          attributes: ['username']
-        },
-        {
-          model: Artist,
-          attributes: ['artist_name']
-        }
-      ]
-    })
-      .then(dbPostData => {
-        if (!dbPostData) {
-          res.status(404).json({ message: 'No post found with this id' });
-          return;
-        }
   
-        // serialize the data
-        const post = dbPostData.get({ plain: true });
-  
-        // pass data to template
-        res.render('edit-post', {
-          post,
-          loggedIn: true
-        });
-      })
-      .catch(err => {
-        console.log('failed to edit posts');
-        res.status(500).json(err);
-      });
-  })
-  
-  module.exports = router;
+module.exports = router;
